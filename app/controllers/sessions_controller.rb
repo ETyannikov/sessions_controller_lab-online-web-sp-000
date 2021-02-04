6e83bd41318489758908cc0d7b8ec3f1307b8989
Eddy Tyannikov <etyannikov@gmail.com>
2021-02-04 18:00:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @name = params[:name]
     if @name != nil
       session[:name] = @name
-      
+      redirect_to controller: 'application', action: 'hello'
     else
       redirect_to controller: 'sessions', action: 'new'
     end
