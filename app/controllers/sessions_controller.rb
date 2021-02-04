378121e88b50f002cb142969424deba62a46e0cd
Eddy Tyannikov <etyannikov@gmail.com>
2021-02-04 18:02:49 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   
   def create
     @name = params[:name]
-    if @name != nil 
+    if !@name
       session[:name] = @name
       redirect_to controller: 'application', action: 'hello'
     else
