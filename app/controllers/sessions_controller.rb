b7f412007e64e37e728c3c90890c1970830d636b
Eddy Tyannikov <etyannikov@gmail.com>
2021-02-04 17:50:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   
   def create
     @name = params[:name]
-    if !!@name
+    if !@name
       session[:name] = @name
     else
       redirect_to login_url
