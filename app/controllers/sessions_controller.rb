e4d9263492dc144783a36c6f37b94733839f38b9
Eddy Tyannikov <etyannikov@gmail.com>
2021-02-04 18:24:25 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,15 @@ class SessionsController < ApplicationController
     @name = params[:name]
     if !@name || @name == ""
       redirect_to controller: 'sessions', action: 'new'
-      binding.pry
     else
       session[:name] = @name
       redirect_to controller: 'application', action: 'hello'
-      binding.pry
     end
   end
   
   def destroy
-    
+    session.destroy
+    redirect_to controller: 'sessions', action: 'new'
   end
   
 end
